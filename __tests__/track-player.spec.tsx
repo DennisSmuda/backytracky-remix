ba7e375ed0ee3fd097e389e1ee437fea159f5bc2
Dennis Smuda <smuda.dennis@gmail.com>
2022-07-27 00:25:17 +0200

    update readme + coverage

--- __tests__/track-player.spec.tsx
+++ __tests__/track-player.spec.tsx
@@ -3,32 +3,33 @@ import { render } from "@testing-library/react";
 
 import TrackPayer from "../app/components/track/TrackPlayer.client";
 
-jest.mock("tone", () => {
-  return {
-    Sampler: jest.fn().mockImplementation(() => {
-      return { toDestination: jest.fn() };
-    }),
-    Synth: jest.fn().mockImplementation(() => {
-      return { toDestination: jest.fn() };
-    }),
-    Transport: jest.fn().mockImplementation(() => {
-      return {
-        start: jest.fn(),
-        loop: jest.fn(),
-        stop: jest.fn((x) => x),
-        dispose: jest.fn(),
-      };
-    }),
-    Part: jest.fn().mockImplementation(() => {
-      return {
-        start: jest.fn(),
-        loop: jest.fn(),
-        stop: jest.fn(),
-        dispose: jest.fn(),
-      };
-    }),
-  };
-});
+jest.mock("tone", () => ({
+  Sampler: jest.fn().mockImplementation(() => {
+    return { toDestination: jest.fn() };
+  }),
+  Synth: jest.fn().mockImplementation(() => {
+    return { toDestination: jest.fn() };
+  }),
+  Transport: jest.fn().mockImplementation(() => {
+    return { stop: jest.fn() };
+  }),
+  // Transport: jest.fn().mockImplementation(() => {
+  //   return {
+  //     start: jest.fn(),
+  //     loop: jest.fn(),
+  //     stop: jest.fn((x) => x),
+  //     dispose: jest.fn(),
+  //   };
+  // }),
+  Part: jest.fn().mockImplementation(() => {
+    return {
+      start: jest.fn(),
+      // loop: jest.fn(),
+      stop: jest.fn(),
+      dispose: jest.fn(),
+    };
+  }),
+}));
 
 describe("Track Player Component", () => {
   it("renders correctly and shows a play button", () => {
