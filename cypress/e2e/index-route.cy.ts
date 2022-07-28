2120faa0f1798d0935feaeccccc94736294625dd
Dennis Smuda <smuda.dennis@gmail.com>
2022-07-29 00:10:18 +0200

    test: fix for new index page button text

--- cypress/e2e/index-route.cy.ts
+++ cypress/e2e/index-route.cy.ts
@@ -1,7 +1,7 @@
 describe("Index Route", () => {
   it("can click on a link to see all tracks", () => {
     cy.visit("/");
-    cy.findByRole("link", { name: /All Tracks/ }).click();
+    cy.findByRole("link", { name: /all the Tracks/i }).click();
     cy.url().should("include", "/tracks");
   });
 });
