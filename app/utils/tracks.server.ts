e58bde619bbb28560de83d9c441bc6b8009a880e
Dennis Smuda <smuda.dennis@gmail.com>
2022-07-30 23:46:31 +0200

    feat: delete tracks

--- app/utils/tracks.server.ts
+++ app/utils/tracks.server.ts
@@ -1,3 +1,5 @@
+import type { Track } from "@prisma/client";
+import { json } from "@remix-run/node";
 import { db } from "./db.server";
 
 export async function createTrack(
@@ -9,4 +11,20 @@ export async function createTrack(
     data: { name: trackname, sheet: chords, upvotes: 0, userId: userId },
   });
   return track;
+}
+
+export async function testTrackFunction() {
+  return true;
+}
+
+export async function deleteTrack(trackId: string) {
+  console.log("Delete Track Server!!");
+  try {
+    const deletedTrack = await db.track.delete({
+      where: { id: trackId },
+    });
+    return json({ message: "Delete Success", track: deletedTrack });
+  } catch {
+    return json({ message: "Delete Error" }, { status: 400 });
+  }
 }
