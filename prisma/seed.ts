9207b8f405682fa072c2d86aabc2d8367a10be1b
Dennis Smuda <smuda.dennis@gmail.com>
2022-08-06 17:05:02 +0200

    remove seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,19 +2,11 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function seed() {
-  const user = await db.user.create({
-    data: {
-      username: "dennis",
-      // this is a hashed version of "twixrox"
-      passwordHash:
-        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
-    },
-  });
   await Promise.all(
     getTracks().map((track) => {
       const data = {
         ...track,
-        userId: user.id,
+        userId: "123-321-123",
         upvotes: 0,
       };
 
@@ -29,11 +21,10 @@ function getTracks() {
   return [
     {
       name: "two five one",
-      sheet: "empty sheet",
-    },
-    {
-      name: "turnaround",
-      sheet: "empty sheet",
+      description: "two five one",
+      authorName: "testuser",
+      bpm: 69,
+      sheet: {},
     },
   ];
 }
