864b58089023cf66e3402ec60772450da3caf148
Dennis Smuda <smuda.dennis@gmail.com>
2022-08-06 23:01:14 +0200

    feat: gooves as functions investigate

--- app/components/track/TrackPlayer.client.tsx
+++ app/components/track/TrackPlayer.client.tsx
@@ -11,10 +11,10 @@ export default function TrackPlayer({ sheet, bpm = 120 }: any) {
   const [isPlaying, setIsPlaying] = useState<Boolean>(false);
   const [, setIsReady] = useState<Boolean>(false);
   const [currentBpm, setCurrentBpm] = useState<number>();
+  const [currentGroove, setCurrentGroove] = useState<string>("hihat");
 
   const piano = useRef<Sampler | null>(null);
   const drums = useRef<Sampler | null>(null);
-  const music = new Music({ sheet });
 
   let chordsPart = useRef<Part | null>(null);
   let chordsPartChords = useRef<Array<ChordBeat> | null>(null);
@@ -27,8 +27,6 @@ export default function TrackPlayer({ sheet, bpm = 120 }: any) {
 
     piano.current = pianoSampler;
     drums.current = drumSampler;
-
-    setupMusic();
   }, []);
 
   useEffect(() => {
@@ -45,47 +43,60 @@ export default function TrackPlayer({ sheet, bpm = 120 }: any) {
   }, [currentBpm]);
 
   useEffect(() => {
-    setIsReady(true);
-  }, [chordsPart, drumPart]);
+    console.log("Set Groove!", currentGroove);
+    // if (Transport.bpm && typeof currentBpm !== "undefined") {
+    //   Transport.bpm.value = currentBpm;
+    // }
+    function setupMusic(): void {
+      const music = new Music({ sheet });
+      const { chords, groove, loopEndTime } =
+        music.generateMusic(currentGroove);
 
-  function setupMusic(): void {
-    const { chords, groove, loopEndTime } = music.generateMusic();
+      console.log("Groove Generated", groove);
 
-    chordsPart.current = new Part(function (time, note) {
-      document
-        .querySelector(`button.sheet-grid__chord.active`)
-        ?.classList.remove("active");
+      chordsPart.current = new Part(function (time, note) {
+        document
+          .querySelector(`button.sheet-grid__chord.active`)
+          ?.classList.remove("active");
 
-      const activeElement = document.querySelector(
-        `button.sheet-grid__chord.bar-${note.bar}.beat-${note.beat}`
-      );
+        const activeElement = document.querySelector(
+          `button.sheet-grid__chord.bar-${note.bar}.beat-${note.beat}`
+        );
 
-      activeElement?.classList.add("active");
-      piano?.current?.triggerAttackRelease(
-        note.note,
-        note.duration,
-        time,
-        0.35
-      );
-    }, chords);
+        activeElement?.classList.add("active");
+        piano?.current?.triggerAttackRelease(
+          note.note,
+          note.duration,
+          time,
+          0.35
+        );
+      }, chords);
 
-    chordsPart.current.start(0);
-    chordsPart.current.loop = true;
-    chordsPart.current.loopEnd = loopEndTime;
-    chordsPartChords.current = chords;
+      chordsPart.current.start(0);
+      chordsPart.current.loop = true;
+      chordsPart.current.loopEnd = loopEndTime;
+      chordsPartChords.current = chords;
 
-    drumPart.current = new Part(function (time, note) {
-      drums?.current?.triggerAttackRelease(
-        note?.note,
-        note?.duration,
-        time,
-        0.2
-      );
-    }, groove);
-    drumPart.current.start(0);
-    drumPart.current.loop = true;
-    drumPart.current.loopEnd = loopEndTime;
-  }
+      drumPart.current = new Part(function (time, note) {
+        drums?.current?.triggerAttackRelease(
+          note?.note,
+          note?.duration,
+          time,
+          0.2
+        );
+      }, groove);
+      drumPart.current.start(0);
+      drumPart.current.loop = true;
+      // drumPart.current.loopEnd = loopEndTime;
+    }
+    stop();
+    disposeParts();
+    setupMusic();
+  }, [currentGroove, sheet]);
+
+  // useEffect(() => {
+  //   setIsReady(true);
+  // }, [chordsPart, drumPart]);
 
   function disposeParts() {
     chordsPart?.current?.dispose();
@@ -127,6 +138,11 @@ export default function TrackPlayer({ sheet, bpm = 120 }: any) {
 
       <div className="form fixed bottom-0 md:bottom-8 left-0 right-0">
         <div className="max-w-2xl mx-auto grid grid-cols-6 gap-4 p-4 bg-zinc-100 dark:bg-gray-1000 rounded-t-lg md:rounded-b-lg">
+          <button onClick={() => setCurrentGroove("hihat")}>hihat</button>
+          <button onClick={() => setCurrentGroove("fourOnTheFloor")}>
+            4 on the Floor
+          </button>
+          <button onClick={() => setCurrentGroove("bossa")}>Bossa</button>
           <label htmlFor="bpm-slider" className="flex flex-col col-span-4">
             <span>bpm: {currentBpm}</span>
             <input
--- app/music/Music.ts
+++ app/music/Music.ts
@@ -24,13 +24,12 @@ export default class Music {
     }
   }
 
-  generateMusic() {
+  generateMusic(currentGroove: string) {
     const groove: Array<{
       time: string;
       note: string | Array<string>;
       duration: Subdivision;
     }> = [];
-    let totalBeatCount = 0;
 
     const chords = this.sheet.map((chord: ChordBeat) => {
       return new ChordBeat(chord);
@@ -38,15 +37,17 @@ export default class Music {
 
     chords.forEach((chord: ChordBeat) => {
       const bars = convertDurationToBeats(chord.duration);
-      totalBeatCount += bars;
       for (let i = 0; i < bars; i++) {
         const next = increaseChordTimeByBeats(chord, i);
-        groove.push({
-          time: `${next.bar}:${next.beat}`,
-          // note: next.beat === 0 ? ["C1", "D1"] : "D1",
-          note: "D1",
-          duration: "8n",
-        });
+        const nextGrooveBeats = this.getNextGrooveBeats(next, currentGroove);
+        // groove.push({
+        //   time: `${next.bar}:${next.beat}`,
+        //   // note: next.beat === 0 ? ["C1", "D1"] : "D1",
+        //   note: this.getDrumBeatNotes(currentGroove, next.bar, next.beat),
+        //   duration: "8n",
+        // });
+        console.log("Groove", nextGrooveBeats);
+        groove.push(...nextGrooveBeats);
       }
     });
 
@@ -58,5 +59,55 @@ export default class Music {
       groove,
       loopEndTime,
     };
+  }
+
+  getNextGrooveBeats(
+    next: { bar: number; beat: number },
+    groove: string = "hihat"
+  ) {
+    return [
+      {
+        time: `${next.bar}:${next.beat}:0`,
+        note: this.getDrumBeatNotes(groove, next.bar, next.beat),
+        duration: "16n" as Subdivision,
+      },
+      // In Betweeners
+      groove === "bossa"
+        ? {
+            time: `${next.bar}:${next.beat}:1`,
+            note: (next.beat + 4) % 4 === 0 ? "C1" : "D1",
+            duration: "16n" as Subdivision,
+          }
+        : {
+            // 16nth hihat
+            time: `${next.bar}:${next.beat}:1`,
+            note: "D1",
+            duration: "16n" as Subdivision,
+          },
+    ];
+  }
+
+  getDrumBeatNotes(groove: string = "hihat", bar: number, beat: number) {
+    if (groove === "hihat") {
+      return "D1";
+    }
+
+    if (groove === "fourOnTheFloor") {
+      return beat % 2 === 1 ? ["C1", "D1", "E1"] : ["C1", "D1"];
+    }
+
+    if (groove === "bossa") {
+      if (bar === 0 && beat === 0) {
+        return ["C1", "D1"];
+      }
+      if (bar === 0 && beat === 1) {
+        return ["D1", "E1"];
+      }
+      if (bar === 0 && beat === 2) {
+        return ["D1"];
+      }
+    }
+
+    return "D1";
   }
 }
