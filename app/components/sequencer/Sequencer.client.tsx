087856f90fae954e2e8c3e70c8dd1d4af484de21
Dennis Smuda <smuda.dennis@gmail.com>
2022-08-11 19:55:29 +0200

    feat: drumbeat sequencer for one bar working

--- app/components/sequencer/Sequencer.client.tsx
+++ app/components/sequencer/Sequencer.client.tsx
@@ -5,34 +5,66 @@ import PlayButton from "../PlayButton";
 
 const timeBeats = [
   "0:0:0",
+  "0:0:1",
   "0:0:2",
+  "0:0:3",
   "0:1:0",
+  "0:1:1",
   "0:1:2",
+  "0:1:3",
   "0:2:0",
+  "0:2:1",
   "0:2:2",
+  "0:2:3",
   "0:3:0",
+  "0:3:1",
   "0:3:2",
-  "1:0:0",
-  "1:0:2",
-  "1:1:0",
-  "1:1:2",
-  "1:2:0",
-  "1:2:2",
-  "1:3:0",
-  "1:3:2",
+  "0:3:3",
+  // "1:0:0",
+  // "1:0:1",
+  // "1:0:2",
+  // "1:0:3",
+  // "1:1:0",
+  // "1:1:1",
+  // "1:1:2",
+  // "1:1:3",
+  // "1:2:0",
+  // "1:2:1",
+  // "1:2:2",
+  // "1:2:3",
+  // "1:3:0",
+  // "1:3:1",
+  // "1:3:2",
+  // "1:3:3",
 ];
 
 export default function Sequencer() {
   const [instruments] = useInstruments();
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [kicks, setKicks] = useState<{ [key: string]: boolean }>({});
+  const [snares, setSnares] = useState<{ [key: string]: boolean }>({});
+  const [hihats, setHihats] = useState<{ [key: string]: boolean }>({});
   let drumPart = useRef<Part | null>(null);
 
   const clickKick = (time: string) => {
     kicks[time] = !kicks[time];
-    // console.log("Time", kickTimes);
     setKicks({ ...kicks });
     instruments?.drumSampler.triggerAttackRelease("C1", "1n", now(), 0.3);
+    stopSequence();
+  };
+
+  const clickSnare = (time: string) => {
+    snares[time] = !snares[time];
+    setSnares({ ...snares });
+    instruments?.drumSampler.triggerAttackRelease("E1", "1n", now(), 0.3);
+    stopSequence();
+  };
+
+  const clickHihat = (time: string) => {
+    hihats[time] = !hihats[time];
+    setHihats({ ...hihats });
+    instruments?.drumSampler.triggerAttackRelease("D1", "1n", now(), 0.3);
+    stopSequence();
   };
 
   const playSequence = (): void => {
@@ -41,6 +73,20 @@ export default function Sequencer() {
       if (kicks[beatTime]) {
         groove.push({
           note: "C1",
+          duration: "4n",
+          time: beatTime,
+        });
+      }
+      if (snares[beatTime]) {
+        groove.push({
+          note: "E1",
+          duration: "4n",
+          time: beatTime,
+        });
+      }
+      if (hihats[beatTime]) {
+        groove.push({
+          note: "D1",
           duration: "4n",
           time: beatTime,
         });
@@ -59,7 +105,7 @@ export default function Sequencer() {
     }, groove);
     drumPart.current.start(0);
     drumPart.current.loop = true;
-    drumPart.current.loopEnd = 4;
+    drumPart.current.loopEnd = 2;
     // Transport.swing = 1;
     setIsPlaying(true);
     if (typeof Transport.start !== "undefined") Transport.start();
@@ -69,22 +115,55 @@ export default function Sequencer() {
   function stopSequence(): void {
     setIsPlaying(false);
     if (typeof Transport.stop !== "undefined") Transport.stop();
+    disposeParts();
+  }
+
+  function disposeParts() {
+    // chordsPart?.current?.dispose();
+    drumPart?.current?.dispose();
+    // bassLinePart?.current?.dispose();
   }
 
   return (
     <div>
       <h2>Sequencer</h2>
-      {JSON.stringify(kicks)}
+      {/* {JSON.stringify(kicks)} */}
+      <div className="sequencer-row">
+        {timeBeats.map((hihatTime) => (
+          <button
+            className={`button button--hihat ${
+              hihats[hihatTime] === true ? "button--active" : ""
+            }`}
+            key={`kick-${hihatTime}`}
+            onClick={() => clickHihat(hihatTime)}
+          >
+            <span className="sr-only">hihat</span>
+          </button>
+        ))}
+      </div>
+      <div className="sequencer-row">
+        {timeBeats.map((snareTime) => (
+          <button
+            className={`button button--snare ${
+              snares[snareTime] === true ? "button--active" : ""
+            }`}
+            key={`kick-${snareTime}`}
+            onClick={() => clickSnare(snareTime)}
+          >
+            <span className="sr-only">snare drum</span>
+          </button>
+        ))}
+      </div>
       <div className="sequencer-row">
         {timeBeats.map((kickTime) => (
           <button
-            className={`button ${
+            className={`button button--kick ${
               kicks[kickTime] === true ? "button--active" : ""
             }`}
             key={`kick-${kickTime}`}
             onClick={() => clickKick(kickTime)}
           >
-            K
+            <span className="sr-only">kick drum</span>
           </button>
         ))}
       </div>
