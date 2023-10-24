e354bbd0e39b82d26d1432696992031ab34f4950
Dennis Smuda <smuda.dennis@gmail.com>
2023-10-25 00:22:15 +0200

    fix: local build

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+
+declare global {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+  }
+  interface Process {
+    env: ProcessEnv;
+  }
+  let process: Process;
+}
+
