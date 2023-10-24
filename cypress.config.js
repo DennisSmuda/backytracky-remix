1dd89595f8a9b316935d66b39fc7e045cb798154
Dennis Smuda <smuda.dennis@gmail.com>
2023-10-25 00:33:34 +0200

    fix: cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,5 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
